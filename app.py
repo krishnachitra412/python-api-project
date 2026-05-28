ace4388583375cb51fc3d132649b8bdf8e6a12d0
Krishnachitra412 <krishnachitra412@gmail.com>
2026-05-28 19:16:40 +0530

    Updated for list and dictoionary

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ import requests
 
 response = requests.get("https://jsonplaceholder.typicode.com/users")
 
-print(response.json())
+data = response.json()
+print(data[0]["name"])
+print(data[1]["email"])
+print(data[2]["phone"])
