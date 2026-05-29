a010be26a40693d694dfa4961b128a60a7700767
Krishnachitra412 <krishnachitra412@gmail.com>
2026-05-29 11:12:04 +0530

    Combined List and Dict

---
+++ employees.py
@@ -0,0 +1,8 @@
+employees = [
+    {"name": "Krishna", "salary": 50000},
+    {"name": "Anu", "salary": 70000},
+    {"name": "Rahul", "salary": 45000}
+]
+
+for emp in employees:
+    print(emp["name"], emp["salary"])
